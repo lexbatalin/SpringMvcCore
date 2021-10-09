ebc91fa1dd9fee16955c7115b78c91023347ba5c
Lex <alexbtln@gmail.com>
2021-10-09 19:38:05 +0300

    add xml response

--- src/main/java/ru/lexbatalin/model/User.java
+++ src/main/java/ru/lexbatalin/model/User.java
@@ -5,10 +5,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@XmlRootElement
+@XmlType(propOrder = {"name", "password", "isAdmin"})
 public class User {
 
     @Size(min = 4, message = "Имя должно быть больше 4 знаков")
